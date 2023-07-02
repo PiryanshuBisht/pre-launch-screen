e61f4c77701de41e1e0d9702880f991912aba0a9
PiryanshuBisht <piryanshu.boxofic.devteam@gmail.com>
2023-07-02 12:53:38 +0530

    adding content

--- src/component/Prelauch.jsx
+++ src/component/Prelauch.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import styles from "./styles/PreLauch.module.css";
 import imgBanner from "./pre-launch-page.png";
 import icon1 from "./icon1.png";
+import wallet from "./wallet.png";
+import share from "./share.png";
+import movie from "./movie.png";
+import ticket from "./ticket.png";
 export const Prelauch = () => {
   return (
     <div className={styles.pre_launch_container}>
@@ -12,38 +16,37 @@ export const Prelauch = () => {
         </div>
         <div className={styles.pre_launch_points}>
           <div>
-            <img src={icon1} alt="" height={"25px"} />
+            <img src={wallet} alt="" height={"25px"} />
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-              sapiente architecto
+              Sign up early and get a 100 rupee credit! Be one of our early bird
+              members.
+            </p>
+          </div>
+          <div>
+            <img src={share} alt="" height={"25px"} />
+            <p>
+              Share the excitement, unlock rewards! Invite loved ones, enjoy
+              benefits.
+            </p>
+          </div>
+          <div>
+            <img src={movie} alt="" height={"25px"} />
+            <p>
+              Enjoy your favorite movies and shows without wasteful
+              subscriptions.
             </p>
           </div>
           <div>
             <img src={icon1} alt="" height={"25px"} />
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-              sapiente architecto
+              Escape subscription hassle. Stream hassle-free, commitment-free.
             </p>
           </div>
           <div>
-            <img src={icon1} alt="" height={"25px"} />
+            <img src={ticket} alt="" height={"25px"} />
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-              sapiente architecto
-            </p>
-          </div>
-          <div>
-            <img src={icon1} alt="" height={"25px"} />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-              sapiente architecto
-            </p>
-          </div>
-          <div>
-            <img src={icon1} alt="" height={"25px"} />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-              sapiente architecto
+              Revolutionize your entertainment experience: Embrace
+              pay-as-you-watch, unlocking maximum value!
             </p>
           </div>
         </div>
